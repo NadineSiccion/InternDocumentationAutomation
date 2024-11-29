c7667be7be5ee95249f178a9b149cf1459346dbe
Nadine Siccion <nv.siccion@gmail.com>
2024-11-29 21:04:07 +0800

    Implement fully functional app and main script

--- AMA/classes.py
+++ AMA/classes.py
@@ -5,25 +5,31 @@ from docx import Document
 from docx.shared import Inches
 from pathlib import Path
 from docx.shared import Pt
+import inspect
+
+# FIXME: make so date is really only a string during saving and loading, any other time, it should be a date
 
 class Internship():
-    def __init__(self, week_no:str, required_hrs:str, completed_hrs:str, start_date:str, current_week_start:str):
+    def __init__(self, week_no:int, required_hrs:int, completed_hrs:int, start_date:date, current_week_start):
         self.week_no = int(week_no)
         self.required_hrs = int(required_hrs)
         self.completed_hrs = int(completed_hrs)
-        self.start_date = start_date
-        self.current_week_start = current_week_start
+        self.start_date = Internship.str_to_date(start_date)
+        self.current_week_start = Internship.str_to_date(current_week_start)
 
     def date_to_str(date:date) -> str:
         return date.strftime("%B %d, %Y")
     
-    def get_week_dates(self, week_start:str)->list[date]:
+    def str_to_date(datestring:str) -> date:
+        temp = str(datestring).split('-')
+        return date(int(temp[0]), int(temp[1]), int(temp[2]))
+    
+    def get_week_dates(self, week_start:date)->list[date]:
         """Returns a list of 5 consecutive days starting from a given date YYYY/MM/DD"""
         temp_list = []
-        week_start_date = Internship.str_to_date(week_start)
         for i in range(5):
-            temp_list.append(week_start_date)
-            week_start_date += timedelta(days=1)
+            temp_list.append(week_start)
+            week_start += timedelta(days=1)
         
         week_dates = []
         for day in temp_list:
@@ -31,15 +37,16 @@ class Internship():
         return week_dates
     
     def get_remaining_hrs(self) -> int:
+        """Return Required_Hours - Completed_Hours"""
         return self.required_hrs - self.completed_hrs
     
-    def account_absent(self, hours:int = 8) -> None:
-        self.completed_hrs - hours
+    def add_absent(self, hours:int = 6) -> None:
+        self.completed_hrs -= hours
         print('Deducted ' + str(hours) + 
               ' hours from completed hours.')
     
-    def account_diff_hours(self, hours:int) -> None:
-        self.completed_hrs - hours
+    def add_hour_difference(self, hours:int) -> None:
+        self.completed_hrs += hours
         if hours > 0:
             print("Added " + str(hours) + 
                   " hours to completed hours")
@@ -47,43 +54,140 @@ class Internship():
             print('Deducted ' + str(hours) + 
               ' hours from completed hours.')
     
-    def increment_week_no(self, num:int = 1) -> None:
+    def update_week_no(self, num:int = 1) -> None:
         self.week_no += num
         print('Week no. incremented by ' + str(num))
+
+    def compute_week_number(self, week_start):
+        week_start = Internship.str_to_date(week_start)
+        week_no = 0
+        counter_week = self.start_date
+        while counter_week < week_start:
+            counter_week += timedelta(weeks=1)
+            week_no += 1
+        
+        return week_no
     
-    def str_to_date(datestring:str) -> date:
-        temp = str(datestring).split('-')
-        return date(int(temp[0]), int(temp[1]), int(temp[2]))
+    def update_internship(self):
+        # add 1 to week_no
+        self.week_no += 1
+        # add 30 hrs to completed hrs
+        self.completed_hrs += (6*5)
+        # add 1 week to current start week
+        self.current_week_start += timedelta(weeks=1)
+        print('Updated data from Week No. ',self.week_no -1, ' to ', self.week_no, 
+              '. Ready to generate next report.')
 
 class Report(Internship):
-    def __init__(self, week_no:str, required_hrs:str, completed_hrs:str, start_date:str, current_week_start, template_file, out_file, save_file):
+    def __init__(self, week_no:int, required_hrs:int, completed_hrs:int, start_date:date, current_week_start, template_file:Path, out_file:Path, save_file:Path):
         super().__init__(week_no, required_hrs, completed_hrs, start_date, current_week_start)
-        self.template_file = Path(template_file)
-        self.out_file = Path(out_file)
-        self.save_file = Path(save_file)
+        self.template_file = template_file
+        self.out_file = out_file
+        self.save_file = save_file
+        
+    # == SAVE AND LOAD FUNCTIONS ==
 
-    
-    def save(self) -> None:
+    def save_report(self) -> None:
         """Saves the current report object as a json in save location."""
-        attributes = [a for a in dir(self) if not a.startswith('__')]
-
-        save_data = {}
-
-        for attribute in attributes:
-            save_data[attribute] = getattr(self, attribute)
+        save_data = {
+            'week_no': str(self.week_no), 
+            'required_hrs': str(self.required_hrs), 
+            'completed_hrs': str(self.completed_hrs), 
+            'start_date': str(self.start_date),
+            'current_week_start': str(self.current_week_start),
+            'template_file': str(self.template_file),
+            'out_file': str(self.out_file),
+            'save_file': str(self.save_file)
+        }
+        
+        print("Generating save data...")
+        for key, value in save_data.items():
+            print(key, ": ", type(value))
         print('Save data generated.')
         
-        with open(self.save_file) as file:
+        with open(self.save_file, "w") as file:
             json.dump(save_data, file, indent=4)
-        print('Report data saved to ' + self.save_file + ".")
+        print('Report data saved to ', self.save_file, ".")
+    
+    def load_report(file_name): #FIXME
+        """Loads report data from a save file. Returns Report object."""
+        try:
+            with open(file_name, 'r') as save_file:
+                data = json.load(save_file)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return 0
+        
+        
+        week_no = int(data['week_no']) 
+        required_hrs = int(data['required_hrs']) 
+        completed_hrs = int(data['completed_hrs']) 
+        start_date = Internship.str_to_date(data['start_date'])
+        current_week_start = Internship.str_to_date(data['current_week_start'])
+        template_file = Path(data['template_file'])
+        out_file = Path(data['out_file'])
+        save_file = Path(data['save_file'])
+        
+        report = Report(
+            week_no, 
+            required_hrs, 
+            completed_hrs, 
+            start_date, 
+            current_week_start, 
+            template_file, 
+            out_file, 
+            save_file)
+        
+        return report
+    
+    # == REPORT GENERATING FUNCTIONS ==
+
+    def generate_data(self, week_start:str = None, hours_per_day:int = 6, days_per_week:int = 5) -> dict:
+        """Returns a dict of the data to be used in making the new document"""
+        self.current_week_start = week_start
+
+        start_date = self.start_date
+        if (week_start != None):
+            week_start = Internship.str_to_date(week_start)
+        else: 
+            week_start = self.current_week_start
+        weekdays = self.get_week_dates(week_start)
+        week_end = weekdays[-1]
+
+        week_no = self.week_no
+        week_hrs = hours_per_day*days_per_week
+        week_mins = week_hrs*60
+        
+        data_dict = {
+        '{week_no}': str(week_no),
+        '{week_start}': Internship.date_to_str(week_start),
+        '{week_end}': str(week_end),
+        '{day_1}': weekdays[0],
+        '{day_2}': weekdays[1],
+        '{day_3}': weekdays[2],
+        '{day_4}': weekdays[3],
+        '{day_5}': weekdays[4],
+        '{week_hrs}': str(week_hrs),
+        '{week_mins}': f'{week_mins:,}',
+        '{remaining_hrs}': str(self.get_remaining_hrs())
+        }
+
+        print('Output: \n')
+        for key, value in data_dict.items():
+            print(f"{key}: {value}")
+
+        return data_dict
 
     def turn_bold(paragraph):
         for run in paragraph.runs:
             run.bold = True
     
-    def edit_document(self):
+    def edit_document(self, days_in_week=None):
         doc = Document(self.template_file)
-        data = self.generate_data(str(self.current_week_start))
+        if days_in_week == None: 
+            data = self.generate_data(str(self.current_week_start))
+        else:
+            data = self.generate_data(str(self.current_week_start), days_per_week=days_in_week)
 
         style = doc.styles['Normal']
         font = style.font
@@ -123,44 +227,6 @@ class Report(Internship):
         
         new_filename = str(self.out_file.name).replace("###", f"{self.week_no:03d}")
         with open(new_filename, 'w') as out_file:
-            doc.save(self.out_file.with_name(new_filename))
+            doc.save(self.out_file.with_name(new_filename)) # There's a bug here but the sofware works fine
         
         print('Saved output at ' + str(new_filename) + '.')
-    
-    def generate_data(self, week_start:str) -> dict:
-        """Returns a dict of the data to be used in making the new document"""
-        
-        start_date = Internship.str_to_date(self.start_date)
-        week_start = Internship.str_to_date(week_start)
-        
-        weekdays = self.get_week_dates(week_start)
-        week_end = weekdays[-1]
-
-        week_no = 0
-        counter_week = start_date
-        while counter_week < week_start:
-            counter_week += timedelta(weeks=1)
-            week_no += 1
-        
-        week_hrs = 6*5
-        week_mins = week_hrs*60
-        
-        data_dict = { # TODO: Edit this to conform to the template document
-        '{week_no}': str(week_no),
-        '{week_start}': Internship.date_to_str(week_start),
-        '{week_end}': str(week_end),
-        '{day_1}': weekdays[0],
-        '{day_2}': weekdays[1],
-        '{day_3}': weekdays[2],
-        '{day_4}': weekdays[3],
-        '{day_5}': weekdays[4],
-        '{week_hrs}': str(week_hrs),
-        '{week_mins}': f'{week_mins:,}',
-        '{remaining_hrs}': str(self.get_remaining_hrs())
-        }
-
-        print('Output: \n')
-        for key, value in data_dict.items():
-            print(f"{key}: {value}")
-
-        return data_dict
--- AMA/main.py
+++ AMA/main.py
@@ -4,15 +4,27 @@ from pathlib import Path
 
 if __name__ == "__main__":
     cwd = Path(os.getcwd())
-    print('Current working directory: ', cwd)
-    start_date = "2024-9-23"
-    last_week_start = "2024-09-23"
+    # print('Current working directory: ', cwd)
+    # start_date = "2024-9-23"
+    # last_week_start = "2024-11-18"
     out_path = cwd / 'outputs' / 'output###.docx'
-    print('Outpath: ', out_path)
+    # print('Outpath: ', out_path)
     template_path = cwd / 'template' / 'AMA_template.docx'
-    print('Template path: ', template_path)
+    # print('Template path: ', template_path)
     save_path = cwd / 'save-data.json'
-    print('Save path: ', save_path)
+    # print('Save path: ', save_path)
 
-    ama = classes.Report("1", "600", "0", start_date, last_week_start, template_path, out_path, save_path)
+    # ama = classes.Report("1", "600", "0", start_date, last_week_start, template_path, out_path, save_path)
+    # ama.generate_data(last_week_start)
+    # ama.save_report()
+    # ama = classes.Report.load_report("save-data.json")
+    # ama.generate_data(last_week_start)
+
+    # ama = classes.Report(1, 600, 0, "2024-09-23", "2024-09-23", template_path, out_path, save_path)
+    ama = classes.Report.load_report(save_path)
+    ama.update_internship()
     ama.edit_document()
+    # if absent, uncomment:
+    # ama.add_hour_difference(-6)
+    # ama.edit_document(days_in_week=4)
+    ama.save_report()
